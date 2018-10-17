08960b0f758cdfa16802bd53c7e89bfed7ce9199
Pavel Pavlov <insoft@gmail.com>
2018-10-17 14:22:54 +0300

    fix: accent color was purple

--- app_in_mail/lib/constants/colors.dart
+++ app_in_mail/lib/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 ///Here we will keep references to colors like a style
 class AppColors {
-  static final accentColor = Color.fromRGBO(218, 34, 80, 1.0);
+  static final accentColor = Color(0xFFF33E7D);
   static final greyLight = Color(0xFFA4A5BF);
   static final darkSplash = Colors.black45;
   static final lightSplash = Colors.white;
