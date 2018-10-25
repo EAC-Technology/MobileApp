5c0a885aaf1012a7208615823ebfce3c7ecd2bef
Pavel Pavlov <insoft@gmail.com>
2018-10-25 17:11:34 +0300

    sub screens from info menu now done. Pending content.

---
+++ app_in_mail/lib/screens/menu/general_info_screen.dart
@@ -0,0 +1,80 @@
+import 'package:app_in_mail/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+enum InfoPageTypes {
+  contactAndSupport, faq, privacyPolicyAndCookies, sofrwareLicense, terms
+}
+
+class GeneralInfoScreen extends StatefulWidget {
+  final InfoPageTypes pageType;
+  final String title;
+  
+  GeneralInfoScreen({this.pageType, this.title});
+
+  @override
+  _GeneralInfoScreenState createState() => _GeneralInfoScreenState();
+}
+
+class _GeneralInfoScreenState extends State<GeneralInfoScreen> {
+  
+  AppBar _buildAppBar() {
+    return new AppBar(
+      backgroundColor: AppColors.toolbar,
+      title: new Text(
+        widget.title,
+        style: TextStyle(
+            color: AppColors.titleTextColor,
+            fontSize: 22.0,
+            fontWeight: FontWeight.bold),
+      ),
+      elevation: 0.5,
+      leading: new IconButton(
+          icon: new Icon(
+            Icons.arrow_back,
+            color: AppColors.titleTextColor,
+            size: 32.0,
+          ),
+          onPressed: () => Navigator.of(context).pop()),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildContent(),
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: const EdgeInsets.all(40),
+      child: Text(_getContent(), style: TextStyle( color: AppColors.greyLight),),
+    );
+
+  }
+
+  //TODO: get actual content here - either from resources or a web link.
+  String _getContent() {
+    String sampleString;
+    switch (widget.pageType) {
+      
+      case InfoPageTypes.contactAndSupport:
+        sampleString = "Contact and support...";
+        break;
+        case InfoPageTypes.faq:
+        sampleString = "Faq ...";
+        break;
+          case InfoPageTypes.privacyPolicyAndCookies:
+        sampleString = "Privacy ....";
+        break;
+          case InfoPageTypes.sofrwareLicense:
+        sampleString = "License ...";
+        break;
+          case InfoPageTypes.terms:
+        sampleString = "Terms ....";
+        break;
+    }
+    return sampleString + 'Lorem ipsum dolor sin amet.Lorem ipsum dolor sin amet.Lorem ipsum dolor sin amet.Lorem ipsum dolor sin amet.Lorem ipsum dolor sin amet.Lorem ipsum dolor sin amet.Lorem ipsum dolor sin amet.Lorem ipsum dolor sin amet.Lorem ipsum dolor sin amet.Lorem ipsum dolor sin amet.Lorem ipsum dolor sin amet.Lorem ipsum dolor sin amet.Lorem ipsum dolor sin amet.Lorem ipsum dolor sin amet.';
+  }
+
+}
--- app_in_mail/lib/screens/menu/info_action_list.dart
+++ app_in_mail/lib/screens/menu/info_action_list.dart
@@ -1,5 +1,5 @@
-
 import 'package:app_in_mail/constants/strings/string_keys.dart';
+import 'package:app_in_mail/screens/menu/general_info_screen.dart';
 import 'package:app_in_mail/screens/menu/menu_item_view.dart';
 import 'package:app_in_mail/utils/localization.dart';
 import 'package:flutter/material.dart';
@@ -10,23 +10,47 @@ class InfoActionList extends StatefulWidget {
 }
 
 class _InfoActionListState extends State<InfoActionList> {
+  void _navigateToInfoPage(InfoPageTypes infoPageType, String title) {
+    Navigator.of(context).push(
+      new MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          return GeneralInfoScreen(
+            pageType: infoPageType,
+            title: title,
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         MenuItemView(
+          onTap: () => _navigateToInfoPage(InfoPageTypes.contactAndSupport,
+              Localization.getString(Strings.contactAndSupport)),
           title: Localization.getString(Strings.contactAndSupport),
         ),
         MenuItemView(
+          onTap: () => _navigateToInfoPage(
+              InfoPageTypes.faq, Localization.getString(Strings.faq)),
           title: Localization.getString(Strings.faq),
         ),
         MenuItemView(
+          onTap: () => _navigateToInfoPage(
+              InfoPageTypes.privacyPolicyAndCookies,
+              Localization.getString(Strings.privacyPolicyAndCookies)),
           title: Localization.getString(Strings.privacyPolicyAndCookies),
         ),
         MenuItemView(
+          onTap: () => _navigateToInfoPage(InfoPageTypes.sofrwareLicense,
+              Localization.getString(Strings.softwareLicense)),
           title: Localization.getString(Strings.softwareLicense),
         ),
         MenuItemView(
+          onTap: () => _navigateToInfoPage(
+              InfoPageTypes.terms, Localization.getString(Strings.terms)),
           title: Localization.getString(Strings.terms),
         ),
       ],
