b186292130e925daf5fda7f280583cefe3707b38
Pavel Pavlov <insoft@gmail.com>
2018-11-03 12:00:31 +0200

    custom slider for the chart page

--- app_in_mail/lib/screens/e_wallet/chart_screen.dart
+++ app_in_mail/lib/screens/e_wallet/chart_screen.dart
@@ -1,41 +1,13 @@
-/// Timeseries chart example
+import 'package:app_in_mail/constants/colors.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-class ChartScreen extends StatelessWidget {
+class ChartScreen extends StatefulWidget {
   final List<charts.Series> seriesList;
 
   ChartScreen(this.seriesList);
 
-  ///TODO:  Remove mocked data , when  we implement the bloc for the real data.
-  factory ChartScreen.withSampleData() {
-    return new ChartScreen(
-      _createSampleData(),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(height: 60,), 
-        _buildChart(),
-      ],
-    );
-  }
-
-  Widget _buildChart() {
-    return Container(
-      height: 300,
-      child: charts.TimeSeriesChart(
-        seriesList,
-        animate: true,
-        dateTimeFactory: const charts.LocalDateTimeFactory(),
-      ),
-    );
-  }
-
-  /// Todo: bellow is a sample hardcoded data , until we provide the real data with bloc.
   static List<charts.Series<TimeSeriesValues, DateTime>> _createSampleData() {
     final data = [
       new TimeSeriesValues(new DateTime(2017, 9, 19), 5),
@@ -54,6 +26,65 @@ class ChartScreen extends StatelessWidget {
       )
     ];
   }
+
+  ///TODO:  Remove mocked data , when  we implement the bloc for the real data.
+  factory ChartScreen.withSampleData() {
+    return new ChartScreen(
+      _createSampleData(),
+    );
+  }
+
+  @override
+  _ChartScreenState createState() => _ChartScreenState();
+}
+
+class _ChartScreenState extends State<ChartScreen> {
+  double slliderValue = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 60,
+        ),
+        _buildChart(),
+        Container(
+          height: 100,
+          child: SliderTheme(
+            child: Slider(
+              label: '23 hours', // slliderValue.toString(),
+              divisions: 6,
+              min: 0,
+              max: 6,
+              onChanged: (double value) {
+                setState(() {
+                  slliderValue = value;
+                });
+              },
+              value: slliderValue,
+            ),
+            data: Theme.of(context).sliderTheme.copyWith(
+                  thumbShape: _CustomThumbShape(),
+                  inactiveTrackColor: AppColors.accentColor,
+                  //thumbShape: _CustomThumbShape(),
+                  //valueIndicatorShape: _CustomValueIndicatorShape(),
+                ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildChart() {
+    return Container(
+      height: 300,
+      child: charts.TimeSeriesChart(
+        widget.seriesList,
+        animate: true,
+        dateTimeFactory: const charts.LocalDateTimeFactory(),
+      ),
+    );
+  }
 }
 
 /// Sample time series data type.
@@ -62,4 +93,68 @@ class TimeSeriesValues {
   final int values;
 
   TimeSeriesValues(this.time, this.values);
+}
+
+class _CustomThumbShape extends SliderComponentShape {
+  static const double _thumbSize = 4.0;
+  static const double _disabledThumbSize = 3.0;
+
+  @override
+  Size getPreferredSize(bool isEnabled, bool isDiscrete) {
+    return isEnabled
+        ? const Size.fromRadius(_thumbSize)
+        : const Size.fromRadius(_disabledThumbSize);
+  }
+
+  static final Animatable<double> sizeTween = Tween<double>(
+    begin: _disabledThumbSize,
+    end: _thumbSize,
+  );
+
+  @override
+  void paint(
+    PaintingContext context,
+    Offset thumbCenter, {
+    Animation<double> activationAnimation,
+    Animation<double> enableAnimation,
+    bool isDiscrete,
+    TextPainter labelPainter,
+    RenderBox parentBox,
+    SliderThemeData sliderTheme,
+    TextDirection textDirection,
+    double value,
+  }) {
+    final Canvas canvas = context.canvas;
+    final thumbSide = 35;
+    final borderRadius = 8.0;
+    
+    final RRect borderRect = RRect.fromLTRBAndCorners(
+      thumbCenter.dx - thumbSide /2,
+      thumbCenter.dy - thumbSide / 2,
+      thumbCenter.dx + thumbSide /2,
+      thumbCenter.dy + thumbSide /2,
+      bottomLeft: Radius.circular(borderRadius),
+      bottomRight: Radius.circular(borderRadius),
+      topLeft: Radius.circular(borderRadius),
+      topRight: Radius.circular(borderRadius),
+    );
+
+    Path shadowPath = Path();
+    shadowPath.addRRect(borderRect);
+    canvas.drawShadow(shadowPath, Colors.grey[100], 10, false);
+
+    Paint backgroundPaint =  Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+    canvas.drawRRect(borderRect, backgroundPaint);
+
+
+    Paint foregroundPaint =  Paint()
+      ..color = AppColors.accentColor
+      ..style = PaintingStyle.fill;
+    canvas.drawCircle(thumbCenter, 8, foregroundPaint);
+    
+   
+    
+  }
 }
